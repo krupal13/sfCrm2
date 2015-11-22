f2dc2a5563f605d7d9175737441d55fd0e73461a
krupal13 <raf.krupinski@gmail.com>
2015-11-22 11:10:27 +0100

    More work on REST API + menu trans

--- src/AppBundle/Menu/MainMenu.php
+++ src/AppBundle/Menu/MainMenu.php
@@ -17,27 +17,33 @@ class MainMenu extends ContainerAware {
     }
 
     public function menu(FactoryInterface $factory, array $options) {
+        
+        $trans = $this->container->get('translator');
+        
         $menu = $factory->createItem('root');
 
-        $menu->addChild('Panel', [
+        $menu->addChild('panel', [
             'route' => 'homepage',
+            'label' => $trans->trans('Panel'),
         ]);
 
         $menu->addChild('agreement_list', [
-            'label' => 'Lista umów',
+            'label' => $trans->trans('Lista umów'),
         ]);
         $menu['agreement_list']->addChild(
                 'Umowy na życie', [
             'route' => 'agreememt_life_list',
+          'label' => $trans->trans('Umowy na życie')
                 ]
         );
         //...
         $menu->addChild('agreement_add', [
-            'label' => 'Nowa umowa',
+            'label' => $trans->trans('Nowa umowa'),
         ]);
         $menu['agreement_add']->addChild(
                 'Umowy na życie', [
             'route' => 'agreememt_life_add',
+            'label' => $trans->trans('Umowy na życie'),
                 ]
         );
         //...
@@ -45,12 +51,14 @@ class MainMenu extends ContainerAware {
         if ($this->container->get('security.authorization_checker')->isGranted('ROLE_MANAGER')) {
             $menu->addChild('Lista agentów', [
                 'route' => 'agents_list',
+                'label' => $trans->trans('Lista agentów')
             ]);
         }
 
         if ($this->container->get('security.authorization_checker')->isGranted('ROLE_MANAGER')) {
             $menu->addChild('Dodaj klienta', [
                 'route' => 'client_add',
+                'label' => $trans->trans('Dodaj klienta'),
             ]);
         }
             return $menu;
